4b958eab0f9c7cf6db1ffb2c7e94aaaaee3ed5e3
aryan <aryantaheri@gmail.com>
2011-12-19 23:03:39 +0100

    add monitor bot

---
+++ examples/monitor.py
@@ -0,0 +1,32 @@
+from jabberbot import JabberBot, botcmd
+import datetime
+
+class SystemInfoJabberBot(JabberBot):
+    @botcmd
+    def serverinfo( self, mess, args):
+        """Displays information about the server"""
+        version = open('/proc/version').read().strip()
+        loadavg = open('/proc/loadavg').read().strip()
+
+        return '%s\n\n%s' % ( version, loadavg, )
+    
+    @botcmd
+    def time( self, mess, args):
+        """Displays current server time"""
+        return str(datetime.datetime.now())
+
+    @botcmd
+    def rot13( self, mess, args):
+        """Returns passed arguments rot13'ed"""
+        return args.encode('rot13')
+
+    @botcmd
+    def whoami(self, mess, args):
+        """Tells you your username"""
+        return mess.getFrom().getStripped()
+
+username = 'test3@localhost'
+password = 'test3'
+bot = SystemInfoJabberBot(username,password)
+bot.serve_forever()
+print 'Aliveeeeeeeeeee'
